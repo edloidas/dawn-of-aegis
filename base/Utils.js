ccc75e4e6059f5a263aaaaad3517d61c1cff76c2
edloidas <edloidas@gmail.com>
2013-11-11 23:39:05 +0300

    Added extend method to Utils.

--- base/Utils.js
+++ base/Utils.js
@@ -7,5 +7,18 @@
 ===============================================================================
 */
 
+function extend(Child, Parent) {
+    var F = function () {}
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.prototype.constructor = Child;
+    Child.superclass = Parent.prototype;
+}
+
+/*
+-------------------------------------------------------------------------------
+    MATH
+-------------------------------------------------------------------------------
+*/
 Math.PI_2 = Math.PI / 2;
 Math.PI2  = Math.PI * 2;
