05ec1db7c7744cfa833a2adc2996ce685a33f582
edloidas <edloidas@gmail.com>
2013-11-14 17:03:56 +0300

    Added UI camera rotation.y as PI rad.

--- base/UI.js
+++ base/UI.js
@@ -11,6 +11,14 @@ function UI() {
     this.camera = new THREE.OrthographicCamera( DOA.Settings.width / - 2, DOA.Settings.width / 2,
                                                 DOA.Settings.height / 2, DOA.Settings.height / - 2,
                                                 DOA.Settings.minView, DOA.Settings.maxView );
+    /*
+     * If camera rotation on Y is 0, not PI radian, all objects visible by
+     * camera must have negative Z coordinates. In this case, the backside
+     * of the object is visible only, and Material.side should be set to
+     * THREE.DoubleSide, so the object can be drawn.
+     */
+    this.camera.rotateY( Math.PI );
+
     this.scene = new THREE.Scene();
 }
 
