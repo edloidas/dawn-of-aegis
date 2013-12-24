268201eae72edb6f4773bb582b4b4301d1fb9eca
edloidas <edloidas@gmail.com>
2013-12-24 23:49:21 +0300

    Update TextTexture Object.
    Replace `updateText` method with `update` and `forcedUpdate` methods.

--- base/objects/TextTexture.js
+++ base/objects/TextTexture.js
@@ -15,83 +15,107 @@ Objects.prototype.TextTexture = function ( text, param ) {
     this.context = this.canvas.getContext( '2d' );
 
     // initialization
-    text = text || "";
-    param = param || {};
+    this.text = text || "";
+    this.param = param;
 
-    param.fontface = param.fontface || DOA.Settings.fontface;
-    param.fontweight = param.fontweight || "normal";
-    param.fontsize = param.fontsize || 24;
+    this.initParam = function ( param ) {
+        param = param || {};
 
-    param.borderWidth = param.borderWidth || 0;
-    param.borderRadius = param.borderRadius || 0;
+        param.fontface = param.fontface || DOA.Settings.fontface;
+        param.fontweight = param.fontweight || "normal";
+        param.fontsize = param.fontsize || 24;
 
-    param.color = param.color || DOA.Colors.text;
-    param.bgColor = param.bgColor || DOA.Colors.blank;
-    param.borderColor = param.borderColor || DOA.Colors.text;
+        param.borderWidth = param.borderWidth || 0;
+        param.borderRadius = param.borderRadius || 0;
 
-    param.opacity  = param.opacity || 1.0;
-    param.bgOpacity  = param.bgOpacity || 0.0;
-    param.borderOpacity  = param.borderOpacity || 0.0;
+        param.color = param.color || DOA.Colors.text;
+        param.bgColor = param.bgColor || DOA.Colors.blank;
+        param.borderColor = param.borderColor || DOA.Colors.text;
 
-    // for future use
-    this.fontsize = param.fontsize;
-    this.borderWidth = param.borderWidth;
+        param.opacity  = param.opacity || 1.0;
+        param.bgOpacity  = param.bgOpacity || 0.0;
+        param.borderOpacity  = param.borderOpacity || 0.0;
+    };
+    this.initParam( this.param );
 
-    this.context.font = param.fontweight + " " +
-                        param.fontsize + "px " +
-                        param.fontface;
+    this.init = function () {
+        // for future use
+        this.fontsize = this.param.fontsize;
+        this.borderWidth = this.param.borderWidth;
 
-    // background color
-    var color = new THREE.Color( param.bgColor );
-    this.context.fillStyle = "rgba( " + color.r + ", " + color.g + ", " +
-                                        color.b + ", " + param.bgOpacity + " )";
-    // border color
-    color.setHex( param.color );
-    this.context.strokeStyle = "rgba( " + color.r + ", " + color.g + ", " +
-                                          color.b + ", " + param.borderOpacity + " )";
+        this.context.font = this.param.fontweight + " " +
+                            this.param.fontsize + "px " +
+                            this.param.fontface;
 
-    this.context.lineWidth = param.borderWidth;
+        // background color
+        var color = new THREE.Color( this.param.bgColor );
+        this.context.fillStyle = "rgba( " + color.r + ", " + color.g + ", " +
+                                            color.b + ", " + this.param.bgOpacity + " )";
+        // border color
+        color.setHex( this.param.color );
+        this.context.strokeStyle = "rgba( " + color.r + ", " + color.g + ", " +
+                                              color.b + ", " + this.param.borderOpacity + " )";
 
-    // 1.4 - extra height for the text below baseline: g, j, p, q
-    var x = param.borderWidth / 2,
-        y = param.borderWidth / 2,
-        w = this.context.measureText( text ).width + param.borderWidth,
-        h = param.fontsize * 1.4 + param.borderWidth,
-        r = param.borderRadius;
+        this.context.lineWidth = this.param.borderWidth;
 
-    this.context.beginPath();
-    this.context.moveTo( x + r, y );
-    this.context.lineTo( x + w - r, y );
-    this.context.quadraticCurveTo( x + w, y, x + w, y + r );
-    this.context.lineTo( x + w, y + h - r );
-    this.context.quadraticCurveTo( x + w, y + h, x + w - r, y + h );
-    this.context.lineTo( x + r, y + h );
-    this.context.quadraticCurveTo( x, y + h, x, y+ h - r );
-    this.context.lineTo( x, y + r );
-    this.context.quadraticCurveTo( x, y, x + r , y );
-    this.context.closePath();
-    this.context.fill();
-    this.context.stroke();
+        // 1.4 - extra height for the text below baseline: g, j, p, q
+        var x = this.param.borderWidth / 2,
+            y = this.param.borderWidth / 2,
+            w = this.context.measureText( this.text ).width + this.param.borderWidth,
+            h = this.param.fontsize * 1.4 + this.param.borderWidth,
+            r = this.param.borderRadius;
 
-    // text color
-    color.setHex( param.color );
-    this.context.fillStyle = "rgba( " + color.r + ", " + color.g + ", " +
-                                        color.b + ", " + param.opacity + " )";
+        this.context.beginPath();
+        this.context.moveTo( x + r, y );
+        this.context.lineTo( x + w - r, y );
+        this.context.quadraticCurveTo( x + w, y, x + w, y + r );
+        this.context.lineTo( x + w, y + h - r );
+        this.context.quadraticCurveTo( x + w, y + h, x + w - r, y + h );
+        this.context.lineTo( x + r, y + h );
+        this.context.quadraticCurveTo( x, y + h, x, y+ h - r );
+        this.context.lineTo( x, y + r );
+        this.context.quadraticCurveTo( x, y, x + r , y );
+        this.context.closePath();
+        this.context.fill();
+        this.context.stroke();
 
-    this.context.fillText( text, param.borderWidth, param.fontsize + param.borderWidth );
+        // text color
+        color.setHex( this.param.color );
+        this.context.fillStyle = "rgba( " + color.r + ", " + color.g + ", " +
+                                            color.b + ", " + this.param.opacity + " )";
+
+        this.context.fillText( this.text, this.param.borderWidth,
+                               this.param.fontsize + this.param.borderWidth );
+
+        this.needsUpdate = true;
+    };
+    this.init();
 
     // canvas contents will be used for a texture
     this.image = this.canvas;
-    this.needsUpdate = true;
 
-    this.updateText = function ( text ) {
-        // Need to redraw bg and border.
+    this.update = function ( text, param, isForced ) {
+        isForced = isForced || false;
+
         this.context.save();
         this.context.setTransform( 1, 0, 0, 1, 0, 0 );
         this.context.clearRect( 0, 0, this.canvas.width, this.canvas.height );
         this.context.restore();
-        this.context.fillText( text, this.borderWidth, this.fontsize + this.borderWidth );
-        this.needsUpdate = true;
+
+        if ( isForced ) {
+            this.initParam( param );
+            this.param = param;
+        } else {
+            // do copy
+            mixin( this.param, param );
+        }
+
+        this.text = text || this.text;
+        this.init();
+    };
+
+    this.forcedUpdate = function ( text, param ) {
+        this.update( text, param, true );
     };
 
     this.getWidth = function () {
